1f80ce3f46384b3d7c7ef505e2e054ee81d3e330
IOSHED <iveninvala7@gmail.com>
2023-10-14 21:51:32 +0300

    #4 Feat replace "" on ''

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,12 @@ from .views import CreateUserView, LoginUserView, CheckEmailView, CheckPasswordV
     CreateInfoUserView, LogoutUserView, DeleteUserView, UpdateUserView, CheckHaveEmailView
 
 urlpatterns = [
-    path('create-user/', CreateUserView.as_view(), name="create-user"),
-    path('create-info-user/<int:pk>', CreateInfoUserView.as_view(), name="create-info-user"),
-    path('login-user/', LoginUserView.as_view(), name="login-user"),
-    path('logout-user/', LogoutUserView.as_view(), name="logout-user"),
-    path('delete-user/', DeleteUserView.as_view(), name="delete-user"),
-    path('update-user/', UpdateUserView.as_view(), name="update-user"),
+    path('create-user/', CreateUserView.as_view(), name='create-user'),
+    path('create-info-user/<int:pk>', CreateInfoUserView.as_view(), name='create-info-user'),
+    path('login-user/', LoginUserView.as_view(), name='login-user'),
+    path('logout-user/', LogoutUserView.as_view(), name='logout-user'),
+    path('delete-user/', DeleteUserView.as_view(), name='delete-user'),
+    path('update-user/', UpdateUserView.as_view(), name='update-user'),
 
 
     # TODO: add forgot password
@@ -17,10 +17,10 @@ urlpatterns = [
 ]
 
 htmx_patterns = [
-    path('check-email/', CheckEmailView.as_view(), name="check-email"),
-    path('check-password/', CheckPasswordView.as_view(), name="check-password"),
-    path('check-re-password/', CheckRePasswordView.as_view(), name="check-re-password"),
-    path('check-have-email/', CheckHaveEmailView.as_view(), name="check-have-email"),
+    path('check-email/', CheckEmailView.as_view(), name='check-email'),
+    path('check-password/', CheckPasswordView.as_view(), name='check-password'),
+    path('check-re-password/', CheckRePasswordView.as_view(), name='check-re-password'),
+    path('check-have-email/', CheckHaveEmailView.as_view(), name='check-have-email'),
 ]
 
 urlpatterns += htmx_patterns
--- accounts/views/check.py
+++ accounts/views/check.py
@@ -9,42 +9,42 @@ from ..models import CustomUser
 class CheckEmailView(View):
     @staticmethod
     def post(request):
-        email = request.POST.get("email")
+        email = request.POST.get('email')
         if CustomUser.objects.filter(email=email).exists():
-            return HttpResponse("This email already exists")
-        return HttpResponse("")
+            return HttpResponse('This email already exists')
+        return HttpResponse('')
 
 
 class CheckHaveEmailView(View):
     @staticmethod
     def post(request):
-        email = request.POST.get("username")
+        email = request.POST.get('username')
         if not CustomUser.objects.filter(email=email).exists():
-            return HttpResponse("There is no user with this email address")
-        return HttpResponse("")
+            return HttpResponse('There is no user with this email address')
+        return HttpResponse('')
 
 
 class CheckPasswordView(View):
     @staticmethod
     def post(request):
-        password1: str = request.POST.get("password1")
+        password1: str = request.POST.get('password1')
         if password1 is None:
-            password1 = request.POST.get("password")
+            password1 = request.POST.get('password')
 
         if len(password1) < 8:
-            return HttpResponse("The password must contain 8 characters")
+            return HttpResponse('The password must contain 8 characters')
         if password1.isnumeric():
-            return HttpResponse("The password must contain Latin letters and numbers")
+            return HttpResponse('The password must contain Latin letters and numbers')
         if not any(c.isupper() for c in password1):
-            return HttpResponse("The password must contain uppercase Latin letters")
-        return HttpResponse("")
+            return HttpResponse('The password must contain uppercase Latin letters')
+        return HttpResponse('')
 
 
 class CheckRePasswordView(View):
     @staticmethod
     def post(request):
-        password1: str = request.POST.get("password1")
-        password2: str = request.POST.get("password2")
+        password1: str = request.POST.get('password1')
+        password2: str = request.POST.get('password2')
         if password1 == password2:
-            return HttpResponse("")
-        return HttpResponse("Passwords must match")
+            return HttpResponse('')
+        return HttpResponse('Passwords must match')
